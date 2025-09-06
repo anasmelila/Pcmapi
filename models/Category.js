26317d158c38746dd4fff415203a51c7f6ec0ce0
anasmelila <33628256+anasmelila@users.noreply.github.com>
2025-09-06 22:55:49 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,4 +1,3 @@
-// models/Category.js
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
@@ -8,17 +7,6 @@ const categorySchema = new mongoose.Schema({
   parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Category", default: null }
 });
 
-// Helper: Calculate level depth recursively
-categorySchema.statics.getCategoryLevel = async function (categoryId) {
-  let level = 1;
-  let current = await this.findById(categoryId).lean();
 
-  while (current && current.parentId) {
-    level++;
-    current = await this.findById(current.parentId).lean();
-  }
-
-  return level;
-};
 
 module.exports = mongoose.model("Category", categorySchema);
